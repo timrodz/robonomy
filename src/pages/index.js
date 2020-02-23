5638c973fdb200825d0a99fd9104b79b0ccd3f06
Juan Alejandro Morais <timrodz@icloud.com>
2020-02-23 23:22:54 +1300

    Fix issue that prevents Gatsby builds
    https://www.gatsbyjs.org/docs/debugging-html-builds/
    
    Load the `styletron-engine-atomic` package dynamically. This is required for every library that uses `window` and `document`.
    
    With `useEffect` we can wait until our component loads and then import the library. Then save engine as a state object.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,29 @@
 import { BaseProvider, LightTheme } from 'baseui';
-import React from 'react';
-import { Client as Styletron } from 'styletron-engine-atomic';
+import React, { useEffect, useState } from 'react';
 import { Provider as StyletronProvider } from 'styletron-react';
 import App from '../components/app';
 
-const engine = new Styletron();
+export default () => {
+  const [engine, setEngine] = useState(null);
 
-export default () => (
-  <StyletronProvider value={engine}>
-    <BaseProvider theme={LightTheme}>
-      <App />
-    </BaseProvider>
-  </StyletronProvider>
-);
+  useEffect(() => {
+    // Load the `styletron-engine-atomic` package dynamically.
+    // Reason: It requires use of `document`, which is not available
+    // outside the browser, so we need to wait until it successfully loads.
+    // Source: https://www.gatsbyjs.org/docs/debugging-html-builds/
+    import('styletron-engine-atomic').then(styletron => {
+      const clientEngine = new styletron.Client();
+      setEngine(clientEngine);
+    });
+  }, []);
+
+  if (!engine) return null;
+
+  return (
+    <StyletronProvider value={engine}>
+      <BaseProvider theme={LightTheme}>
+        <App />
+      </BaseProvider>
+    </StyletronProvider>
+  );
+};
