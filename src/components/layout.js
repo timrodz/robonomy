871407c9e171d4bcca0d942bcbd8db5b20ec74a2
Juan Alejandro Morais <timrodz@icloud.com>
2020-02-25 09:46:47 +1300

    Add Footer component to layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,8 @@
-import { Paragraph1 } from 'baseui/typography';
+import { Block } from 'baseui/block';
 import { graphql, useStaticQuery } from 'gatsby';
 import PropTypes from 'prop-types';
-import { useStyletron } from 'styletron-react';
 import * as React from 'react';
+import Footer from './footer';
 import Header from './header';
 import './layout.css';
 
@@ -18,32 +18,16 @@ const Layout = ({ children }) => {
     }
   `);
 
-  const [css] = useStyletron();
-  const centered = css({ textAlign: 'center' });
-  const link = css({ textDecoration: 'none' });
-
   return (
     <>
       <Header
         siteTitle={data.site.siteMetadata.title}
         email={data.site.siteMetadata.email}
       />
-      <div>
+      <Block>
         <main>{children}</main>
-        <footer>
-          <Paragraph1 className={centered}>
-            Copyright © {new Date().getFullYear()} Roberto Ord&#243;ñez — Made
-            by{' '}
-            <a className={link}
-              href="https://www.timrodz.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Juan Alejandro Morais
-            </a>
-          </Paragraph1>
-        </footer>
-      </div>
+      </Block>
+      <Footer />
     </>
   );
 };
