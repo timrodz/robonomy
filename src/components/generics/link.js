021907b019d6ef500ab8eec7fe1359edb3f9e3c1
Juan Alejandro Morais <timrodz@icloud.com>
2020-02-25 09:45:48 +1300

    Format link.js

--- src/components/generics/link.js
+++ src/components/generics/link.js
@@ -1,12 +1,12 @@
+import { useStyletron } from 'baseui';
 import * as React from 'react';
-import {useStyletron} from 'baseui';
 
 const Link = props => {
   const [css, theme] = useStyletron();
-	const decor = css({
-		color: theme.colors.primaryA,
-		textDecoration: 'none'
-	});
+  const decor = css({
+    color: theme.colors.primaryA,
+    textDecoration: 'none',
+  });
   return <a className={decor} {...props} />;
 };
 
