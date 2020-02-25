63b0ea85525eb3b6144fde6066b8e258740ecfdb
Juan Alejandro Morais <timrodz@icloud.com>
2020-02-25 13:02:17 +1300

    Add data/config.js

---
+++ data/config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  author: 'Juan Alejandro Morais',
+  authorUrl: 'https://www.timrodz.com',
+  title: 'Robonomy',
+  description:
+    'Portfolio of Roberto Ordóñez, Photographer based in Panama City, Panama.',
+  url: 'https:www.robonomy.com',
+  logo: 'static/images/favicon.png',
+  legalName: 'Roberto Ordóñez',
+  socialLinks: {
+    instagram: 'https://www.instagram.com/robonomy',
+    twitter: 'https://twitter.com/robonomy',
+  },
+  social: {
+    instagramId: 'robonomy',
+    twitter: '@robonomy',
+  },
+  googleAnalyticsID: 'UA-XXXXXXXX-X',
+  themeColor: '#000',
+  backgroundColor: '#fff',
+  address: {
+    city: 'Panama City',
+    country: 'Panama',
+  },
+  contact: {
+    email: 'rordnz@gmail.com',
+  },
+  foundingDate: '2020',
+};
